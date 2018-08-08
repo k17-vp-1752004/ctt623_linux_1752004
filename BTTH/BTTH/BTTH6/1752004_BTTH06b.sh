c02defa5f65d0d113855a9739d2dfe0872dc3fdd
k17-vp-1752004 <1752004@student.hcmus.edu.vn>
2018-08-09 06:59:57 +0700

    update

--- BTTH/BTTH/BTTH6/1752004_BTTH06b.sh
+++ BTTH/BTTH/BTTH6/1752004_BTTH06b.sh
@@ -13,12 +13,31 @@ countWord()
 showWord()
 {
     local res=''
-    res=$(('Cac tu co trong file: '))
+    local temp=$#
+    local i=1
     for word in $*
     do
-	res+=$(($word))
+	i=$(($i + 1))
+	res+=$word
+	if ((i <= temp)); then
+	    res+=", "
+	fi
     done
     echo "$res"
+}
+
+countLoopWord() #$1=word $2=all_words_in_file
+{
+    local cnt=0
+    local word=$1
+    for i in $*
+    do
+	if [ "$i" == "$word" ]; then
+	    cnt=$(($cnt + 1))
+	fi
+    done
+    cnt=$(($cnt - 1))
+    echo "Tu $word xuat hien $cnt lan"
 }
 
 process()
@@ -30,7 +49,12 @@ process()
 	str+=$line
 	str+=" "
     done < $fin
-    echo "$str" >> $fout
+    local res=$(countWord $str)
+    echo "So tu co trong file la $res " >> $fout
+    res=$(showWord $str)
+    echo "Cac tu co trong file la: $res " >> $fout
+    res=$(countLoopWord "vong" $str)
+    echo "$res" >> $fout
 }
 
 process inputb.txt outputb.txt
