cc7d0e2210f85d86cc717456cd8bd0bfcdec9cb9
k17-vp-1752004 <1752004@student.hcmus.edu.vn>
2018-08-15 09:43:21 +0700

    update

--- BTTH/BTTH/BTTH8/BTTH08.sh
+++ BTTH/BTTH/BTTH8/BTTH08.sh
@@ -4,10 +4,11 @@ flag_add=false
 flag_delete=false
 flag_search=false
 flag_count=false
+flag_vcard=false
 
 showHelp()
 {
-    echo "$0 [f|--find] [c|--count] [a|--add] [del|--delete] [input]"
+    echo "$0 [f|--find] [c|--count] [a|--add] [del|--delete] [v-|--vcard] [input]"
 }
 
 search() 
@@ -46,9 +47,9 @@ add()
     read phone
     echo -n "Email: "
     read email
-    echo -n "Status: "
-    read status
-    echo -n "$lname $fname $phone $email $status" >> $fin
+    echo -n "Age: "
+    read age
+    echo -n "$lname $fname $phone $email $age" >> $fin
 }
 
 delete()
@@ -74,22 +75,69 @@ delete()
     rm temp.txt
 }
 
+printVCard()
+{
+    local fin=$1
+    local res=''
+    echo "Nhap 1 tu trong muc can xuat: "
+    read word
+    touch directory.vcf
+    chmod u+x directory.vcf
+    local fout="directory.vcf"
+    while read line; do
+	for w in $line; do
+	    if (($w == $word)); then
+		res=line
+		break
+	    fi
+	done
+    done
+    i=0
+    for w in $res; do
+	i=$(($i+1))
+	if (($i==1)); then
+	    echo -n "Last name: " >> $fout
+	fi
+	if (($i==2)); then
+	    echo -n "First name: " >> $fout
+	fi
+	if (($i==3)); then
+	    echo -n "Phone: " >> $fout
+	fi
+	if (($i==4)); then
+	    echo -n "Email: " >> $fout
+	fi
+	if (($i==5)); then
+	    echo -n "Age: " >> $fout
+	fi
+	echo "$w" >> $fout
+	echo >> $fout
+    done
+}
+
 
 showInfos()
 {
     local fin=$1
+    if (($flag_help)); then
+	showHelp
+    fi
     if (($flag_count)); then
 	count $fin
     fi
     if (($flag_search)); then
-	find $fin
+       find $fin
     fi
     if (($flag_add)); then
-	add $fin
+       add $fin
     fi
     if (($flag_delete)); then
-	delete $fin
+       delete $fin
     fi
+    if (($flag_vcard)); then
+        printVCard $fin
+    fi
+
 }
 
 process()
@@ -101,6 +149,8 @@ process()
         s|--search) flag_search=true;;
         a|--add) flag_add=true;;
         d|--delete) flag_delete=true;;
+	c|--count) flag_count=true;;
+	v|--vcard) flag_vcard=true;;
     esac
     showInfos $option
 }
